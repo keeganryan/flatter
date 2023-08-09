500e31df6b7308e8101b2a4a9cc816bf8f483417
Keegan Ryan <kryan@eng.ucsd.edu>
2023-08-09 12:04:47 -0700

    Require minimum version of FPLLL. Fixes #7.

--- cmake/FindFPLLL.cmake
+++ cmake/FindFPLLL.cmake
@@ -3,12 +3,18 @@ find_library(FPLLL_LIBRARY
   NAMES fplll
 )
 
+if(FPLLL_INCLUDE_DIR)
+  set(FPLLL_VERSION "")
+  file(STRINGS "${FPLLL_INCLUDE_DIR}/fplll/fplll_config.h" fplll_version_str REGEX "^#define[ \t]+FPLLL_VERSION[ \t]+")
+  string(REGEX REPLACE "^.*FPLLL_VERSION[ \t]+([0-9.]+)$" "\\1" FPLLL_VERSION "${fplll_version_str}")
+  unset(fplll_version_str)
+endif()
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(FPLLL
   FOUND_VAR FPLLL_FOUND
-  REQUIRED_VARS
-  FPLLL_INCLUDE_DIR
-  FPLLL_LIBRARY
+  REQUIRED_VARS FPLLL_INCLUDE_DIR FPLLL_LIBRARY
+  VERSION_VAR FPLLL_VERSION
 )
 
 if(FPLLL_FOUND)
